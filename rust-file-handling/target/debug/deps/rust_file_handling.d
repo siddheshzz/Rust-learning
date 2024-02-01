ff78e90846968b575cc462d7483890c8e75183b9
siddhesh zope <siddheshzope1023@gmail.com>
2024-02-01 19:24:03 +0530

    file handling

---
+++ rust-file-handling/target/debug/deps/rust_file_handling.d
@@ -0,0 +1,5 @@
+D:\Code\RustLearning\Rust-learning\rust-file-handling\target\debug\deps\rust_file_handling.exe: src\main.rs
+
+D:\Code\RustLearning\Rust-learning\rust-file-handling\target\debug\deps\rust_file_handling.d: src\main.rs
+
+src\main.rs:
---
+++ rust-file-handling/target/debug/rust-file-handling.d
@@ -0,0 +1,1 @@
+D:\Code\RustLearning\Rust-learning\rust-file-handling\target\debug\rust-file-handling.exe: D:\Code\RustLearning\Rust-learning\rust-file-handling\src\main.rs
